fce34bc5fb47f83cf756ba6e7a297e39dfeb1fe6
Hoon <hoon0661@gmail.com>
2025-03-28 12:43:16 -0400

    added feature section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+import Feature from "./components/Feature";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Feature />
     </main>
   );
 };
